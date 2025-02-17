a99a1500fa6c45ca27240dbea2e3320f09cbd6d0
Estevão Bimbo de Lima <rm90691@estudante.fieb.edu.br>
2025-02-17 18:45:51 -0300

    Início Controle de versão

---
+++ src/main/java/com/itb/inf3an25/pizzariacursov001/Pizzariacursov001Application.java
@@ -0,0 +1,13 @@
+package com.itb.inf3an25.pizzariacursov001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pizzariacursov001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pizzariacursov001Application.class, args);
+	}
+
+}
